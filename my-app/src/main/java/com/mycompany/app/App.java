93487d2e0824d5a8257f9efcd408dbe476e08e61
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-02-06 18:46:10 -0800

    parsed csv, stored in string array, need to find better method

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,9 @@
 package com.mycompany.app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String csvPath = "/Users/vvaddadhi/Desktop/Apache-Parquet-OCI/my-app/SalesJan2009.csv";
+        String line = "";
+        String commaSplit = ",";
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))){
+            while ((line = br.readLine()) != null) {
+                String[] city = line.split(commaSplit);
+
+                System.out.println("City = " + city[4]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+    }
     }
 }
