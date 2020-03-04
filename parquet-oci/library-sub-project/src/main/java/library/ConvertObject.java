a6fb5367ca1342ab34af01a41aecc35164e03676
phuuvle <phuuvle@gmail.com>
2020-03-03 16:19:32 -0800

    added convertobject file

---
+++ parquet-oci/library-sub-project/src/main/java/library/ConvertObject.java
@@ -0,0 +1,9 @@
+package library;
+
+import org.apache.parquet.avro.AvroParquetWriter;
+
+public class ConvertObject {
+    public void convertCsvToParquet() {
+        System.out.println("here");
+    }
+}
