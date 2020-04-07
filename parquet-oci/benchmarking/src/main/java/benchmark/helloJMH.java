5d998c8627470cb34d805df003698fd9694462e2
Yibo Guo <yguo25@ucsc.edu>
2020-04-06 20:38:22 -0700

    benchmark added, should work, but not compiling on my Ubuntu

---
+++ parquet-oci/benchmarking/src/main/java/benchmark/helloJMH.java
@@ -0,0 +1,27 @@
+package benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@BenchmarkMode(Mode.All)
+@Warmup(iterations = 5)
+public class helloJMH {
+
+    @Benchmark
+    public void hello() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main (String[] args) {
+        try {
+            new Runner(new OptionsBuilder().include(helloJMH.class.getSimpleName()).forks(1).build()).run();
+        } catch (RunnerException e) {
+            System.out.println("Benchmarking failed.");
+        }
+    }
+}
