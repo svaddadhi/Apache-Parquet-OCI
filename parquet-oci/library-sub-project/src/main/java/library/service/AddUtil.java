315fedd2f3ee5dbb4f184cce484079ab1c0a4ece
Yibo Guo <yguo25@ucsc.edu>
2020-05-25 00:24:01 -0700

    drill unconditional conversion

---
+++ parquet-oci/library-sub-project/src/main/java/library/service/AddUtil.java
@@ -0,0 +1,19 @@
+package library.service;
+
+public class AddUtil {
+
+    private String exeAdd, dbsAdd;
+
+    public AddUtil (String add) {
+        this.exeAdd = add;
+        this.dbsAdd = add;
+    }
+
+    public AddUtil (String exe, String rmt) {
+        this.exeAdd = exe;
+        this.dbsAdd = rmt;
+    }
+
+    public String getAdd () {return this.dbsAdd;}
+    public String getSvc () {return this.exeAdd;}
+}
