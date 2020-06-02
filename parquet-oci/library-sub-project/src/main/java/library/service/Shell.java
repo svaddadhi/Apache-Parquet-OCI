4d6de8c0c61e451c14451a131e6ac45e27839bed
Yibo Guo <yguo25@ucsc.edu>
2020-06-02 00:55:06 -0700

    debugging info removed shell

--- parquet-oci/library-sub-project/src/main/java/library/service/Shell.java
+++ parquet-oci/library-sub-project/src/main/java/library/service/Shell.java
@@ -6,45 +6,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Shell {
-    public static boolean exe(String str) {
-        System.out.println(str);
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("bash", "-c", str);
-
-        Process process = null;
-        try {
-            process = processBuilder.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        StringBuilder output = new StringBuilder();
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while (true) {
-                try {
-                    if (!((line = reader.readLine()) != null)) break;
-                    output.append(line + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        int exitVal = 0;
-        try {
-            exitVal = process.waitFor();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (exitVal == 0) {
-                System.out.print(output);
-                return true;
-            } else {
-                System.out.println("error: " + exitVal);
-                return false;
-            }
+    public static boolean exe(String str) throws IOException, InterruptedException {
+        return new ProcessBuilder().command("bash", "-c", str).start().waitFor() == 0;
     }
 }
